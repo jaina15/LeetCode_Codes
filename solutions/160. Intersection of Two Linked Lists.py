9d5f7484088e2d8e81e1904084489e74593f2c6c
Shubham Jain <shubhamjain151097@gmail.com>
2021-05-26 16:36:14 +0530

    176 ms

--- solutions/160. Intersection of Two Linked Lists.py
+++ solutions/160. Intersection of Two Linked Lists.py
@@ -1,23 +1,3 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-​
-class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
-        s=set()
-        tempa=headA
-        tempb=headB
-        la=0
-        lb=0
-        while(tempa):
-            la+=1
-            tempa=tempa.next
-        
-        while(tempb):
-            lb+=1
-            tempb=tempb.next
         
         if(la<=lb):
             tempa=headA
@@ -44,3 +24,24 @@ class Solution:
                 tempa=tempa.next
                 
         return None
+    
+ #better and optimized solution. Think like this Shubham!!!   
+    #def getIntersectionNode(self, headA, headB):
+    #    if headA is None or headB is None:
+    #        return None
+​
+    #    pa = headA # 2 pointers
+    #    pb = headB
+​
+    #    while pa is not pb:
+    #        # if either pointer hits the end, switch head and continue the second traversal, 
+    #        # if not hit the end, just move on to next
+    #        pa = headB if pa is None else pa.next
+    #        pb = headA if pb is None else pb.next
+​
+     #   return pa # only 2 ways to get out of the loop, they meet or the both hit the end=None
+​
+# the idea is if you switch head, the possible difference between length would be countered. 
+# On the second traversal, they either hit or miss. 
+# if they meet, pa or pb would be the node we are looking for, 
+# if they didn't meet, they will hit the end at the same iteration, pa == pb == None, return either one of them is the same,None
