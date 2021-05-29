9c215868c2226b50548bb34044a78febab9850f1
Shubham Jain <shubhamjain151097@gmail.com>
2021-05-29 18:04:27 +0530

    36 ms

--- solutions/86. Partition List.py
+++ solutions/86. Partition List.py
@@ -31,3 +31,31 @@ class Solution:
             new_node=ListNode(i)
             
             last=new_head
+            while last.next:
+                last=last.next
+            last.next=new_node
+        
+        for i in l2:
+            new_node=ListNode(i)
+            
+            while last.next:
+                last=last.next
+            last.next=new_node
+        
+        return ans.next
+​
+# aisa dimag bhi laga liya kar bhai!!!!
+#def partition(self, head, x):
+#    h1 = l1 = ListNode(0)
+#    h2 = l2 = ListNode(0)
+#    while head:
+#        if head.val < x:
+#            l1.next = head
+#            l1 = l1.next
+#        else:
+#           l2.next = head
+#           l2 = l2.next
+#        head = head.next
+#    l2.next = None
+#    l1.next = h2.next
+#    return h1.next
