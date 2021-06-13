dfaa75a5f388ff9d0526714a129968723b1a04f5
Shubham Jain <shubhamjain151097@gmail.com>
2021-06-13 19:23:52 +0530

    1272 ms

--- solutions/739. Daily Temperatures.py
+++ solutions/739. Daily Temperatures.py
@@ -1,22 +1,19 @@
 class Solution:
-    def dailyTemperatures(self, temp: List[int]) -> List[int]:
-        # bahot samjha re baba ise, tab jaake aaya. Par time complexity toh fir bhi zyada h
-        stack=[]
-        ans=[0]*len(temp)
-        stack.append(0)
-        #print(temp[stack[-1]])
-        for i in range(1,len(temp)):
-            c=0
-            if temp[i]>temp[stack[-1]]:
-                #print('temp i',temp[i])
-                #print('temp stack wala',temp[stack[-1]])
-                while stack and temp[i]>temp[stack[-1]]:
-                    ans[stack[-1]]=i-stack[-1]
-                    stack.pop()
-                stack.append(i)
-            else:
-                stack.append(i)
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        next_greater=[]
+        ans=[i for i in range(len(temperatures))]
+        
+        for i in range(len(temperatures)):
+            while next_greater and temperatures[next_greater[-1]]  < temperatures[i]:
+                #print(i)
+                ans[next_greater[-1]]=i-next_greater[-1]
+                next_greater.pop()
+            next_greater.append(i)
+            #print(ans)
+            #print(next_greater)
+        
+        while next_greater:
+            ans[next_greater.pop()]=0
             
-            #print('stack status',stack)
         #print(ans)
         return ans
