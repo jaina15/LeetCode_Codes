3888c2d5c532493f85a89501947d5926dad05b8d
Shubham Jain <shubhamjain151097@gmail.com>
2021-06-14 13:20:31 +0530

    28 ms

--- solutions/1441. Build an Array With Stack Operations.py
+++ solutions/1441. Build an Array With Stack Operations.py
@@ -1,11 +1,11 @@
 class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
-        #:(
         stack=[]
         d=0
+        
         for i in range(1,target[-1]+1):
             stack.append('Push')
-            if i != target[d]:
+            if i!=target[d]:
                 stack.append('Pop')
             else:
                 d+=1
