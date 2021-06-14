f07ecc046434994c02fa5a824a6b3c6771406836
Shubham Jain <shubhamjain151097@gmail.com>
2021-06-14 13:47:49 +0530

    48 ms

--- solutions/496. Next Greater Element I.py
+++ solutions/496. Next Greater Element I.py
@@ -1,23 +1,17 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # dekh k kia h :(
         stack=[]
-        lookup={}
+        d={}
         
-        for i in nums2:
-            if len(stack)==0:
-                stack.append(i)
-            elif i>stack[-1] and stack:
-                while stack and i>stack[-1]:
-                    lookup[stack.pop()]=i
-                stack.append(i)
-            else:
-                stack.append(i)
+        for i in range(len(nums2)):
+            while stack and nums2[stack[-1]]<=nums2[i]:
+                d[nums2[stack[-1]]]=nums2[i]
+                stack.pop()
+            stack.append(i)
+        #print(d)
+        stack.clear()
+        for i in range(len(nums1)):
+            stack.append(d.get(nums1[i],-1))
         
-        #print(lookup)
-        ans=[]
-        for i in nums1:
-            ans.append(lookup.get(i,-1))
-        
-        return ans
-        
+        #print(stack)
+        return stack
