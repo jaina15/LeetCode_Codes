29d94d3ffd7c9ea385aae55b278b69a507754c3f
Shubham Jain <shubhamjain151097@gmail.com>
2021-06-18 19:42:34 +0530

    52 ms

---
+++ solutions/501. Find Mode in Binary Search Tree.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findMode(self, root: TreeNode) -> List[int]:
+        l=[]
+        ans=dict()
+        self.inorder(root,l)
+        #print(l)
+        if len(l)==1:
+            return l
+        
+        for i in l:
+            if i in ans:
+                ans[i]+=1
+            else:
+                ans[i]=1
+        maxx=max(ans.values())
+        #print(maxx)
+        l=[]
+        for key,value in ans.items():
+            if value==maxx:
+                l.append(key)    
+        #print(l)
+        return l
+        
+    def inorder(self,root,l):
+        if root:
