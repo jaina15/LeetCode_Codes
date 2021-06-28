68642f58452a35648af732773df1a1b1ed76cf87
Shubham Jain <shubhamjain151097@gmail.com>
2021-06-28 15:49:04 +0530

    36 ms

---
+++ solutions/101. Symmetric Tree.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque as queue
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        l,t=[],[]
+        temp1,temp2=root,root
+        lefts,rights=0,0
+        while temp1.left:
+            temp1=temp1.left
+            lefts+=1
+        while temp2.right:
+            temp2=temp2.right
+            rights+=1
+        if lefts!=rights:
+            return False
+        self.inorder(root.left,l)
+        self.reverseinorder(root.right,t)
+        print(l,t)
+        if len(l)!=len(t) or l!=t:
+            return False
+        else:
+            return True
+        
+    def inorder(self,root,l):
+        if root:
+            if root.left or root.right:
+                if root.left:
+                    self.inorder(root.left,l)
+                else:
+                    l.append(None)
+                l.append(root.val)
+                if root.right:
+                    self.inorder(root.right,l)
+                else:
+                    l.append(None)
+            if not root.left and not root.right:
+                l.append(root.val)
