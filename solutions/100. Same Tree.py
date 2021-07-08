bd8b7fdf87eb009b52361d40ac61084570a623af
Shubham Jain <shubhamjain151097@gmail.com>
2021-07-08 19:27:57 +0530

    52 ms

--- solutions/100. Same Tree.py
+++ solutions/100. Same Tree.py
@@ -6,46 +6,8 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        l1=[]
-        l2=[]
-        pls,prs=0,0
-        qls,qrs=0,0
-        plsn,prsn=p,p
-        qlsn,qrsn=q,q
-        while plsn:
-            plsn=plsn.left
-            pls+=1
-        while prsn:
-            prsn=prsn.right
-            prs+=1
-        while qlsn:
-            qlsn=qlsn.left
-            qls+=1
-        while qrsn:
-            qrsn=qrsn.right
-            qrs+=1
-        print(pls,prs,qls,qrs)
-        if pls!=qls or prs!=qrs:
-            return False
-        
-        self.inorder(p,l1)
-        self.inorder(q,l2)
-        
-        if l1==l2:
+        if p is None and q is None:
             return True
-        return False
-    
-    def inorder(self,root,l):
-        if root:
-            if root.left or root.right:
-                if root.left:
-                    self.inorder(root.left,l)
-                else:
-                    l.append(None)
-                l.append(root.val)
-                if root.right:
-                    self.inorder(root.right,l)
-                else:
-                    l.append(None)
-            if not root.left and not root.right:
-                l.append(root.val)
+        if p is None or q is None:
+            return False
+        return p.val==q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
