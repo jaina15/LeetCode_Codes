13bfca94fa7b45da80a41868cda3aeb464a8b142
Shubham Jain <shubhamjain151097@gmail.com>
2021-07-14 09:09:02 +0530

    32 ms

--- solutions/94. Binary Tree Inorder Traversal.py
+++ solutions/94. Binary Tree Inorder Traversal.py
@@ -6,13 +6,34 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: TreeNode) -> List[int]:
-        l=[]
-        self.recursive_call(root,l)
-        return l
+    #one liner    
+    #    return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right) if root else []
     
-    def recursive_call(self,root,l):
-        if root:
-            self.recursive_call(root.left,l)
-            l.append(root.val)
-            self.recursive_call(root.right,l)
-        
+    #recursive
+    #    l=[]
+    #    self.recursive_call(root,l)
+    #    return l
+    
+    #def recursive_call(self,root,l):
+    #    if root:
+    #        self.recursive_call(root.left,l)
+    #        l.append(root.val)
+    #        self.recursive_call(root.right,l)
+     
+    #Morris Traversal
+        curr=root
+        l=[]
+        while curr:
+            if not curr.left:
+                l.append(curr.val)
+                curr=curr.right
+            else:
+                pre=curr.left
+                while pre.right:
+                    pre=pre.right
+                pre.right=curr
+                temp=curr
+                curr=curr.left
+                temp.left=None    
+        return l
+                    
