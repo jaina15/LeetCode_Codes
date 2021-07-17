c029232aa7d0a8141177a183fb90dc1c314ef2db
Shubham Jain <shubhamjain151097@gmail.com>
2021-07-17 14:41:55 +0530

    52 ms

--- solutions/623. Add One Row to Tree.py
+++ solutions/623. Add One Row to Tree.py
@@ -1,3 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def addOneRow(self, root: TreeNode, val: int, depth: int) -> TreeNode:
+        if depth==1:
+            node=TreeNode(val)
+            node.left=root
+            return node
+        else:
             d=self.maxDepth(root)
             print(d)
             if depth>d:
