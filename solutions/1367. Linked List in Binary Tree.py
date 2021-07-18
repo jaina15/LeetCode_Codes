60cc13812f440f12ebfbc13269d68d4d17a2102e
Shubham Jain <shubhamjain151097@gmail.com>
2021-07-18 19:08:17 +0530

    104 ms

--- solutions/1367. Linked List in Binary Tree.py
+++ solutions/1367. Linked List in Binary Tree.py
@@ -1,3 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubPath(self, head: ListNode, root: TreeNode) -> bool:
+        if not head:
+            return True
+        if not root:
+            return False
+        if self.check(head,root):
+            return True
+        return self.isSubPath(head,root.left) or self.isSubPath(head,root.right)
     
     def check(self,head,root):
         if not head:
