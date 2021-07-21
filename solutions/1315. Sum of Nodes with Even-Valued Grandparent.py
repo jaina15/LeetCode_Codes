f4310fb10f91fcdc92852b4451575e95ebc030d5
Shubham Jain <shubhamjain151097@gmail.com>
2021-07-21 14:00:06 +0530

    108 ms

--- solutions/1315. Sum of Nodes with Even-Valued Grandparent.py
+++ solutions/1315. Sum of Nodes with Even-Valued Grandparent.py
@@ -12,13 +12,10 @@ class Solution:
         q=queue()
         q.append([root,None,None])
         q.append(None)
-        l,a=[],[]
         s=0
         while len(q)>0:
             curr=q.popleft()
             if curr is None:
-                l.append(a)
-                a=[]
                 if len(q)==0:
                     break
                 q.append(None)
@@ -27,7 +24,6 @@ class Solution:
                     q.append([curr[0].left,curr[0].val,curr[1]])
                 if curr[0].right:
                     q.append([curr[0].right,curr[0].val,curr[1]])
-                a.append([curr[0].val,curr[2]])
                 if curr[2] is not None and curr[2]%2==0:
                     s+=curr[0].val
         return s
