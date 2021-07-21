503b21839017a68b30ee0e05a23bb623188c1f1d
Shubham Jain <shubhamjain151097@gmail.com>
2021-07-21 13:23:01 +0530

    32 ms

--- solutions/897. Increasing Order Search Tree.py
+++ solutions/897. Increasing Order Search Tree.py
@@ -8,21 +8,12 @@ class Solution:
     def increasingBST(self, root: TreeNode) -> TreeNode:
         l=[]
         self.inorder(root,l)
-        print(l)
-        temp=TreeNode(l[0])
+        ans=temp=TreeNode(l[0])
         for i in range(1,len(l)):
-            self.insert(temp,l[i])
-        return temp
-        
-    def insert(self,root,val):
-        if root is None:
-            root.val=val
-            return
-        else:
-            while root.right:
-                root=root.right
-            root.right=TreeNode(val)
-        
+            temp.right=TreeNode(l[i])
+            temp=temp.right
+        return ans
+            
     def inorder(self,root,l):
         if root:
             self.inorder(root.left,l)
