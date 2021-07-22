afa960b97a1e2a104ea0f13261c01e6c622d457a
Shubham Jain <shubhamjain151097@gmail.com>
2021-07-22 17:27:21 +0530

    36 ms

--- solutions/958. Check Completeness of a Binary Tree.py
+++ solutions/958. Check Completeness of a Binary Tree.py
@@ -4,24 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-#in this we are trying to Flase the node in the tree which will make it incomplete(left child is none and right chile is not none), if we found this we return False, else we are returning True
+#in this we are trying to find the node in the tree which will make it incomplete(left child is none and right chile is not none), if we found this we return False, else we are returning True
 from collections import deque as queue
 class Solution:
     def isCompleteTree(self, root: TreeNode) -> bool:
         if root is None:
             return True
-        check=False
+        have_null =False
         
         q=queue()
         q.append(root)
         while len(q)>0:
             curr=q.popleft()
-            #print(curr)
-            #print('===')
             if not curr:
-                check=True
-                #print(curr,'heloooo')
+                have_null =True
                 continue
-            if check:
+            if have_null :
                 return False
             q.append(curr.left)
+            q.append(curr.right)
+        
+        return True
