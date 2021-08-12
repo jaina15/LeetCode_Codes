6dc175c8b7b0e56127c4204ea03f726dce14b88f
Shubham Jain <shubhamjain151097@gmail.com>
2021-08-12 09:52:55 +0530

    196 ms

--- solutions/547. Number of Provinces.py
+++ solutions/547. Number of Provinces.py
@@ -7,22 +7,16 @@ class Solution:
             for j in range(len(c[i])):
                 if c[i][j]== 1:
                     g[i].append(j)
-        #print(g)
         self.visited=[False]*len(g)
-        self.component=[-1]*len(g)
         self.dfs(0,g)
         for i in range(len(g)):
             if not self.visited[i]:
                 self.cnt+=1
                 self.dfs(i,g)
-        #print(self.cnt)
-        #print(self.component)
-        #return len(set(self.component))
         return self.cnt+1
         
     def dfs(self,v,g):
         self.visited[v]=True
-        self.component[v]=self.cnt
         for neighbour in g[v]:
             if not self.visited[neighbour]:
                 self.dfs(neighbour,g)
