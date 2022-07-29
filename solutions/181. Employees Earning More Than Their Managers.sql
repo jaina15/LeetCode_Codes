ec5ba1b94da85f26fdd8aa3d273c1248b8d253f4
Shubham Jain <shubhamjain151097@gmail.com>
2022-07-29 15:26:03 +0530

    633 ms

--- solutions/181. Employees Earning More Than Their Managers.sql
+++ solutions/181. Employees Earning More Than Their Managers.sql
@@ -1,3 +1,5 @@
 /* Write your PL/SQL query statement below */
-select e.name as employee from employee e inner join employee m 
-on e.managerid=m.id and e.salary>m.salary
+SELECT E.NAME AS EMPLOYEE FROM EMPLOYEE E
+INNER JOIN EMPLOYEE M
+ON E.MANAGERID=M.ID
+WHERE E.SALARY>M.SALARY
