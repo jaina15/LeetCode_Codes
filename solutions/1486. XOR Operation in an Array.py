37298eae85f91e1dc92eb1c499f64beac406f6a5
Shubham Jain <shubhamjain151097@gmail.com>
2023-02-20 14:19:43 +0530

    37 ms

--- solutions/1486. XOR Operation in an Array.py
+++ solutions/1486. XOR Operation in an Array.py
@@ -2,6 +2,5 @@ class Solution:
     def xorOperation(self, n: int, start: int) -> int:
         ans=0
         for i in range(n):
-            ans=ans^(start+2*i)
+            ans^=start+2*i
         return ans
-        
