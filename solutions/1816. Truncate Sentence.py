67a73e58c0c604637c0b5917a28ce4cc7d7e9fab
Shubham Jain <shubhamjain151097@gmail.com>
2023-02-20 14:22:41 +0530

    32 ms

--- solutions/1816. Truncate Sentence.py
+++ solutions/1816. Truncate Sentence.py
@@ -1,11 +1,3 @@
 class Solution:
     def truncateSentence(self, s: str, k: int) -> str:
-        #for i,v in enumerate(s):
-        #    if v == ' ' and k>0:
-        #        k-=1
-        #    if k>=0 and i==len(s)-1:
-        #        return s
-        #    if k==0:
-        #        return s[:i]
-            
-        return " ".join(s.split()[:k])
+        return ' '.join(s.split(' ')[:k])
