7d22ae61852aedc6ef964559a45882e6e250e1eb
Shubham Jain <shubhamjain151097@gmail.com>
2023-02-21 16:10:32 +0530

    52 ms

--- solutions/141. Linked List Cycle.py
+++ solutions/141. Linked List Cycle.py
@@ -6,11 +6,10 @@
 ​
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow,fast = head, head
+        slow,fast=head,head
         while fast and fast.next:
             slow=slow.next
             fast=fast.next.next
-            if slow == fast:
+            if slow==fast:
                 return True
-            
         return False
