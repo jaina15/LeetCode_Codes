a90d8571e33c8b65635c0c5da1dc5f7711c9e0d9
Shubham Jain <shubhamjain151097@gmail.com>
2023-03-03 18:59:14 +0530

    52 ms

--- solutions/1598. Crawler Log Folder.py
+++ solutions/1598. Crawler Log Folder.py
@@ -1,10 +1,12 @@
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
-        ans = 0
+        stack=[]
         for i in logs:
-            if i == '../':
-                if ans>0:
-                    ans-=1
-            elif i != './':
-                ans+=1
-        return ans
+            if i=='./':
+                pass
+            elif i=='../':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(i)
+        return len(stack)
