f4166b0b53507e0264c64ad387113216026b501a
Shubham Jain <shubhamjain151097@gmail.com>
2023-03-04 18:11:26 +0530

    987 ms

--- solutions/196. Delete Duplicate Emails.sql
+++ solutions/196. Delete Duplicate Emails.sql
@@ -2,10 +2,7 @@
  Please write a DELETE statement and DO NOT write a SELECT statement.
  Write your PL/SQL query statement below
  */
-DELETE FROM PERSON WHERE ID IN (
-    SELECT ID FROM (
-        SELECT
-        EMAIL,ID,ROW_NUMBER() OVER(PARTITION BY EMAIL ORDER BY ID) RN
-        FROM PERSON
-    ) WHERE RN>1
-)
+DELETE FROM PERSON WHERE (EMAIL,ID) NOT IN (
+SELECT EMAIL,MIN(ID) AS ID
+FROM PERSON
+GROUP BY EMAIL)
