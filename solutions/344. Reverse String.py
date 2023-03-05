c685ff87ef41b81b0056fa10cbbdba49109d2b80
Shubham Jain <shubhamjain151097@gmail.com>
2023-03-05 14:26:34 +0530

    205 ms

--- solutions/344. Reverse String.py
+++ solutions/344. Reverse String.py
@@ -3,12 +3,9 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        i,j=0,len(s)-1
-        while i<j:
-            t=s[i]
-            s[i]=s[j]
-            s[j]=t
-            
-            i+=1
-            j-=1
-        
+        l,r=0,len(s)-1
+        while l<r:
+            s[l],s[r]=s[r],s[l]
+            l+=1
+            r-=1
+        return s
