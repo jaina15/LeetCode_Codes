ec167daeadfe67b7b872b91f78454b59844ecaa2
Shubham Jain <shubhamjain151097@gmail.com>
2023-03-05 14:39:32 +0530

    120 ms

--- solutions/268. Missing Number.py
+++ solutions/268. Missing Number.py
@@ -1,7 +1,3 @@
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        s=len(nums)*(len(nums)+1)//2
-        summ=0
-        for i in nums:
-            summ+=i
-        return s-summ
+    def missingNumber(self, n: List[int]) -> int:
+        return (len(n)*(len(n)+1))//2-sum(n)
