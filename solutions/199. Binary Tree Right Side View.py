d96967275a84ec7d5a56824a6c97a92dd7827e9f
Shubham Jain <shubhamjain151097@gmail.com>
2023-03-06 16:46:53 +0530

    32 ms

--- solutions/199. Binary Tree Right Side View.py
+++ solutions/199. Binary Tree Right Side View.py
@@ -6,30 +6,25 @@
 #         self.right = right
 from collections import deque as queue
 class Solution:
-    def rightSideView(self, root: TreeNode) -> List[int]:
-        if root is None:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
             return
         q=queue()
         q.append(root)
         q.append(None)
         l,a=[],[]
         while len(q)>0:
-            curr=q.popleft()
+            curr = q.popleft()
             if curr is None:
-                l.append(a)
+                l.append(a[-1])
                 a=[]
                 if len(q)==0:
                     break
                 q.append(None)
-            
             else:
                 if curr.left:
                     q.append(curr.left)
                 if curr.right:
                     q.append(curr.right)
                 a.append(curr.val)
-        
-        for i in range(len(l)):
-            l[i]=l[i][-1]
-        #print(l)
         return l
