200532e4f66e689102e2a48b6d50c82ae9a47740
Shubham Jain <shubhamjain151097@gmail.com>
2023-03-17 14:04:54 +0530

    86 ms

--- solutions/735. Asteroid Collision.py
+++ solutions/735. Asteroid Collision.py
@@ -1,43 +1,14 @@
 class Solution:
-    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+    def asteroidCollision(self, a: List[int]) -> List[int]:
         stack=[]
-        
-        for i in asteroids:
-            if not stack:
-                stack.append(i)
-            else:
-                if stack[-1]<i:
-                    stack.append(i)
-                elif stack[-1]+i>0 and i<0 and stack[-1]>0:
-                    pass
-                elif stack[-1]+i<0 and i<0 and stack[-1]>0:
-                    
-                    while stack[-1]+i<0 and i<0 and stack[-1]>0:
-                        stack.pop()
-                        if not stack:
-                            break
-                        #print(stack)
-                    
-                    stack.append(i)
-                    #print(stack)
-                    if len(stack)>1:
-                        while stack[-2]+stack[-1]>0 and stack[-1]<0 and stack[-2]:
-                            stack.pop()
-                            if len(stack)==1:
-                                break
-                            
-                        while stack[-1]<0 and stack[-2]==-stack[-1] and stack[-2]>0:
-                            #print(stack)
-                            stack.pop()
-                            stack.pop()
-                            #print(stack)
-                            if len(stack)<=1:
-                                break
-                        
-                elif stack[-1]+i==0 and i<0:
+        for i in a:
+            while stack and i < 0 < stack[-1]:
+                if stack[-1] < -i:
                     stack.pop()
-                else:
-                    stack.append(i)
-                    #print(stack)
-                    
+                    continue
+                elif stack[-1] == -i:
+                    stack.pop()
+                break
+            else:
+                stack.append(i)
         return stack
