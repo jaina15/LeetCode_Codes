df555b2c505e3957bd877fdf67b2f32df91a50bb
Shubham Jain <shubhamjain151097@gmail.com>
2023-04-02 11:01:58 +0530

    37 ms

--- solutions/92. Reverse Linked List II.py
+++ solutions/92. Reverse Linked List II.py
@@ -4,69 +4,28 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
-        if left==right:
+    def reverseBetween(self, head: Optional[ListNode], m: int, n: int) -> Optional[ListNode]:
+        if not head or m==n or head.next is None:
             return head
+        dummyNode = ListNode(0)
+        dummyNode.next = head
+        curr = dummyNode
         prev=None
-        revs=None
-        revsprev=None
-        revend=None
-        revendnext=None
-        curr=head
-        initial=head
-        #starting position
-        p=1
-        f=0
-        if(p==1 and left==1):
-            f=1
-        while(curr and p<left):
-            revsprev=curr
+        for i in range(m-1):
+            prev=curr
             curr=curr.next
-            p+=1
-        revs=curr
-        while(curr and p<right):
-            curr=curr.next
-            p+=1
-        revend=curr
-        revendnext=curr.next
-        revend.next=None
-      # print(revs.val)
-       #print(revsprev.val)
-      # print(revend.val)
-      # print(revendnext.val)
         
-        #curr=revs
-        #while(curr is not None):
-        #   print(curr.val,end='->')
-        #    curr=curr.next
-        #urr=revs
-        while(revs is not None):
-            next=revs.next
-            revs.next=prev
-            prev=revs
-            revs=next
-        revs=prev
-        #rint(revs)
-        if f==0:
-            revsprev.next=revs
-            revs=revsprev
-        else:
-            head=revs
-        
-        while(revs.next is not None):
-            revs=revs.next
-        revs.next=revendnext
-#       head=prev
- #      print(head)
- #      if f==0:
- #          revsprev.next=head
- #          head=revsprev
-#       curr=head
-#   #   print(head)
-#       while(curr.next is not None):
-#       #    print(curr.val,end='->')
-#           curr=curr.next
- #      curr.next=revendnext
-        return head
-        
-        
+        new_curr=curr.next
+        curr.next=None
+        point = new_curr
+        prev=None
+        last=None
+        for i in range(n-m+1):
+            last=new_curr
+            nxt=new_curr
+            new_curr=new_curr.next
+            nxt.next=prev
+            prev=nxt
+        curr.next=prev
+        point.next=new_curr
+        return dummyNode.next
