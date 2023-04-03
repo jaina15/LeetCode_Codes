a0708b0f9b4a39f80598516b47b74c2a49d9adb2
Shubham Jain <shubhamjain151097@gmail.com>
2023-04-03 15:54:36 +0530

    36 ms

--- solutions/206. Reverse Linked List.py
+++ solutions/206. Reverse Linked List.py
@@ -7,11 +7,11 @@ class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev=None
         curr=head
-        while head:
-            curr = head
-            head = head.next
+        while curr:
+            nextt = curr.next
             curr.next = prev
             prev = curr
+            curr = nextt
         return prev
     #this is recursive solution
     #    return self.helper(head,None)
