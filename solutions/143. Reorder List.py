42548101b5be9828ac655079b93ce71b7788ffd5
Shubham Jain <shubhamjain151097@gmail.com>
2023-04-03 15:53:08 +0530

    94 ms

--- solutions/143. Reorder List.py
+++ solutions/143. Reorder List.py
@@ -4,38 +4,37 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reorderList(self, head: ListNode) -> None:
+    def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
         """
-        slow=head
-        fast=head
-        while fast.next and fast.next.next:
-            slow=slow.next
-            fast=fast.next.next
-        
-        prev=None
-        curr=slow.next
-        
+        if not head or not head.next:
+            return
+        else:
+            mid = self.getMiddle(head)
+            prev = self.reverse(mid)
+            head1, head2=head,prev
+            while head2:
+                nextt = head1.next
+                head1.next = head2
+                head1 = head2
+                head2 = nextt
+​
+    def reverse(self,head):
+        prev, curr = None, head
         while curr:
-            next=curr.next
-            curr.next=prev
-            prev=curr
-            curr=next
-        slow.next=None
-        
-        
-        #ye tagda logic h, bahot tagda. Samjhana padega ise acche se
-        #ye 2 list ko alternatively merge karne k liye h
-        head1, head2 = head, prev
-        
-        while head2:
-            next=head1.next
-            head1.next=head2
-            #print(head1)
-            #print(head2)
-            head1=head2
-            head2=next
-            #print(head1)
-            #print(head2)
-        
+            nextt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextt
+        return prev
+​
+    def getMiddle(self,head):
+        slow,fast = head,head
+        prev=None
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        prev.next = None
+        return slow
