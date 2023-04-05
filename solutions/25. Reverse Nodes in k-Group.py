98fb386c48c3e01cf0497487726a746a3731d583
Shubham Jain <shubhamjain151097@gmail.com>
2023-04-05 17:19:06 +0530

    46 ms

--- solutions/25. Reverse Nodes in k-Group.py
+++ solutions/25. Reverse Nodes in k-Group.py
@@ -4,52 +4,24 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
-        if k<=1 or head is None or head.next is None:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        curr,count=head,0
+        while curr and count<k:
+            curr=curr.next
+            count+=1
+        if count<k:
             return head
-        
-        temp=head
-        l=0
-        while temp:
-            temp=temp.next
-            l+=1
-        #itti baar reverse karenge
-        times=l//k
-        temp=head
-        dummy=ListNode(0)
-        ans=dummy
-        prev=head
-        while times>0:
-            n=k
-            newhead=prev
-            temp=prev
-            #k nodes k list banane k liye
-            while n>1 and temp:
-                temp=temp.next
-                n-=1
-            prev=temp.next
-            temp.next=None
-            #print(temp)
-            #print(prev)
-            #print(newhead)
-            curr=newhead
-            pr=None
-            while curr:
-                next=curr.next
-                curr.next=pr
-                pr=curr
-                curr=next
-            newhead=pr
-            #print(newhead)
-            while dummy.next:
-                dummy=dummy.next
-            dummy.next=newhead
-            #print(ans.next)
-            times-=1
-        #print(prev)
-        #ye jab hume left out nodes ko bina reverse kie add karna h
-        if prev:
-            while dummy.next:
-                dummy=dummy.next
-            dummy.next=prev
-        return ans.next
+        new_head,prev = self.reverse(head,k)
+        #print(new_head,prev)
+        head.next = self.reverseKGroup(new_head,k)
+        return prev
+    
+    def reverse(self, head, count):
+        curr,prev = head, None
+        while curr and count>0:
+            nextt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextt
+            count-=1
+        return (curr,prev)
