20c78c3134187ded47bdfd1adfd5f2e86fc8e6a2
Shubham Jain <shubhamjain151097@gmail.com>
2023-04-06 19:39:40 +0530

    40 ms

--- solutions/100. Same Tree.py
+++ solutions/100. Same Tree.py
@@ -5,9 +5,12 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        if p is None and q is None:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
             return True
-        if p is None or q is None:
+        if not p or not q:
             return False
-        return p.val==q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+        if p.val!=q.val:
+            return False
+        return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+        
