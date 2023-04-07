82ac34553682f86a20e777f4e1f643b1b5c11b14
Shubham Jain <shubhamjain151097@gmail.com>
2023-04-07 13:01:37 +0530

    76 ms

--- solutions/700. Search in a Binary Search Tree.py
+++ solutions/700. Search in a Binary Search Tree.py
@@ -5,18 +5,23 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        # this is recursive solution
+        # if not root:
+        #     return
+        # if root.val == val:
+        #     return root
+        # elif root.val > val:
+        #     return self.searchBST(root.left, val)
+        # else:
+        #     return self.searchBST(root.right, val)
         
-        if root.val==val:
-            return root
-        
-        if val<root.val:
-            if root.left:
-                return self.searchBST(root.left,val)
+        # this is iterative solution
+        while root:
+            if root.val == val:
+                return root
+            elif root.val > val:
+                root = root.left
             else:
-                return
-        else:
-            if root.right:
-                return self.searchBST(root.right,val)
-            else:
-                return
+                root = root.right
+        return
