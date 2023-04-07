6db92fcf14bd61ed9274bc3048893a6dc09069d5
Shubham Jain <shubhamjain151097@gmail.com>
2023-04-07 17:37:41 +0530

    735 ms

---
+++ solutions/1045. Customers Who Bought All Products.sql
@@ -0,0 +1,6 @@
+/* Write your PL/SQL query statement below */
+select
+customer_id
+from customer
+group by customer_id
+having count(distinct product_key) = (select count(distinct product_key) from product)
