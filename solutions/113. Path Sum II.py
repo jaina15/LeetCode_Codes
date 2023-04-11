b8043ff18a34ef1d99cd6027e3e99f0dddf469fc
Shubham Jain <shubhamjain151097@gmail.com>
2023-04-11 16:17:43 +0530

    55 ms

--- solutions/113. Path Sum II.py
+++ solutions/113. Path Sum II.py
@@ -5,24 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
         l=[]
         s=''
         self.creator(root,l,s,targetSum)
         return l
     
-    def creator(self,root,l,s,targetSum):
-        if root is None:
+    def creator(self, root, l, s, targetSum):
+        if not root:
             return
-        
         s+=str(root.val)+' '
-        #s.append(root.val)
-        #print(s)
-        if root.left is None and root.right is None:
-            #print(s)
+        if not root.left and not root.right:
             l.append(list(map(int, s.split())))
             if sum(l[-1])!=targetSum:
                 l.pop()
-            
         self.creator(root.left,l,s,targetSum)
         self.creator(root.right,l,s,targetSum)
