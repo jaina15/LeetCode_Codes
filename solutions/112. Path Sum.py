0b6932a69a9e5784f09d7883ddfef7dd9c212e39
Shubham Jain <shubhamjain151097@gmail.com>
2023-04-11 16:14:41 +0530

    60 ms

--- solutions/112. Path Sum.py
+++ solutions/112. Path Sum.py
@@ -5,7 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         l=[]
         s=''
         self.creator(root,l,s,targetSum)
@@ -13,17 +13,13 @@ class Solution:
             return True
         return False
     
-    def creator(self,root,l,s,targetSum):
-        if root is None:
+    def creator(self, root, l, s, targetSum):
+        if not root:
             return
-        
         s+=str(root.val)+' '
-        if root.left is None and root.right is None:
-            print(s)
-            l.append(list(map(int, s.split())))
+        if not root.left and not root.right:
+            l.append(list(map(int,s.split())))
             if sum(l[-1])!=targetSum:
                 l.pop()
-            
         self.creator(root.left,l,s,targetSum)
         self.creator(root.right,l,s,targetSum)
-        
