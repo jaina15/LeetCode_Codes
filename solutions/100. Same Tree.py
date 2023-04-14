15403c4e4738f0ad9c9c9ef29ae2a71a99819fcd
Shubham Jain <shubhamjain151097@gmail.com>
2023-04-14 14:16:17 +0530

    32 ms

--- solutions/100. Same Tree.py
+++ solutions/100. Same Tree.py
@@ -10,7 +10,4 @@ class Solution:
             return True
         if not p or not q:
             return False
-        if p.val!=q.val:
-            return False
-        return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
-        
+        return p.val==q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
