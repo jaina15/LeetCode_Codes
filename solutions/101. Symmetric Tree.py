ea53b196e3379cabc888f22f0656a50eac223071
Shubham Jain <shubhamjain151097@gmail.com>
2023-04-14 14:19:03 +0530

    34 ms

--- solutions/101. Symmetric Tree.py
+++ solutions/101. Symmetric Tree.py
@@ -6,11 +6,11 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        return self.isSame(root.left,root.right)
-​
-    def isSame(self,root1,root2):
-        if root1 is None and root2 is None:
+        return self.helper(root.left, root.right)
+    
+    def helper(self, p, q):
+        if not p and not q:
             return True
-        if root1 is None or root2 is None:
+        if not p or not q:
             return False
-        return root1.val==root2.val and self.isSame(root1.left,root2.right) and self.isSame(root1.right,root2.left)
+        return p.val==q.val and self.helper(p.left,q.right) and self.helper(p.right,q.left)
