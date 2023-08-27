19a02307db8d9e63a99c032e1815b1aef9846857
Shubham Jain <shubhamjain151097@gmail.com>
2023-08-27 17:19:04 +0530

    56 ms

--- solutions/2114. Maximum Number of Words Found in Sentences.py
+++ solutions/2114. Maximum Number of Words Found in Sentences.py
@@ -2,5 +2,5 @@ class Solution:
     def mostWordsFound(self, sentences: List[str]) -> int:
         maxi=0
         for s in sentences:
-            maxi=max(maxi,len(s.split(" ")))
+            maxi = max(maxi,len(s.split()))
         return maxi
