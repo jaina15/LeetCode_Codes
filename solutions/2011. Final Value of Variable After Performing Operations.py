44f52aab72931f57c03ea83e44b0c75e31da5f9e
Shubham Jain <shubhamjain151097@gmail.com>
2023-08-27 17:12:53 +0530

    63 ms

--- solutions/2011. Final Value of Variable After Performing Operations.py
+++ solutions/2011. Final Value of Variable After Performing Operations.py
@@ -1,9 +1,9 @@
 class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
-        x=0
-        for i in operations:
-            if i in ('X++','++X'):
-                x+=1
-            elif i in ('X--','--X'):
+        x = 0
+        for o in operations:
+            if o in ['--X','X--']:
                 x-=1
+            elif o in ['X++','++X']:
+                x+=1
         return x
