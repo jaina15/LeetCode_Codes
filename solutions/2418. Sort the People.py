f3cc88dde1f14e6226a827ffa9860da33a67e13f
Shubham Jain <shubhamjain151097@gmail.com>
2023-11-02 11:33:31 +0530

    114 ms

--- solutions/2418. Sort the People.py
+++ solutions/2418. Sort the People.py
@@ -1,3 +1,5 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        return [i[0] for i in sorted(zip(names,heights),key=lambda x:x[1],reverse=True)]
+        li = [(names[i],heights[i]) for i in range(len(names))]
+        li.sort(key = lambda x:x[1],reverse=True)
+        return [li[i][0] for i in range(len(li))]
