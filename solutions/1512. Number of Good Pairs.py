a73d3031a840c612ca9a04236ed14af2f8b1b110
Shubham Jain <shubhamjain151097@gmail.com>
2023-11-02 11:00:13 +0530

    26 ms

--- solutions/1512. Number of Good Pairs.py
+++ solutions/1512. Number of Good Pairs.py
@@ -1,13 +1,8 @@
+from collections import Counter
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        ans=0
-        d=dict()
-        for i in nums:
-            if i in d:
-                d[i]+=1
-            else:
-                d[i]=1
-        
-        for k,v in d.items():
-            ans+=(v*(v-1))//2
+        cnt = Counter(nums)
+        ans = 0
+        for k,v in cnt.items():
+            ans+=(v*(v-1)//2)
         return ans
