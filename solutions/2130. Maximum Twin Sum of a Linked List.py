dbd48d8cc4448c261cdcd059a62ad8b503e248fc
Shubham Jain <shubhamjain151097@gmail.com>
2023-11-05 15:01:28 +0530

    647 ms

--- solutions/2130. Maximum Twin Sum of a Linked List.py
+++ solutions/2130. Maximum Twin Sum of a Linked List.py
@@ -5,40 +5,27 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        init,slow,fast=head,head,head
-        last,prev=None,None
+        slow,fast,prev = head,head,None
         
         while fast and fast.next:
-            prev=slow
-            slow=slow.next
-            fast=fast.next.next
-        second=slow
-        prev.next=None
-        #print(init)
-        #print(second)
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        prev.next = None
+        curr = slow
+        pre = None
+        while curr:
+            nextt = curr.next
+            curr.next = pre
+            pre = curr
+            curr= nextt
         
-        pre=None
-        while second:
-            curr=second
-            second=second.next
-            curr.next=pre
-            pre=curr
-        #print(pre)
+        second = pre
+        first = head
         
-        ans=-1
-        while init:
-            ans=max(ans,init.val+pre.val)
-            init=init.next
-            pre=pre.next
-        return ans
-#6
-#0   6-1-0   5
-#1   6-1-1   4
-#2   6-1-2   3
-#3   6-1-3   2
-#4   6-1-4   1
-#5   6-1-5   0
-​
-#6   8   5   1   2   4
-​
-​
+        maxi=0
+        while first:
+            maxi = max(maxi,first.val+second.val)
+            first = first.next
+            second = second.next
+        return maxi
