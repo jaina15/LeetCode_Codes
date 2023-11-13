dc9890959eeb8d896b1cffe343cc6b5a00f05557
Shubham Jain <shubhamjain151097@gmail.com>
2023-11-13 10:47:19 +0530

    329 ms

--- solutions/1669. Merge In Between Linked Lists.py
+++ solutions/1669. Merge In Between Linked Lists.py
@@ -5,20 +5,23 @@
 #         self.next = next
 class Solution:
     def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
-        prev,check_a,check_b=None,None,None
-        init,head=list1,list1
-        c=0
-        while list1:
-            if a==c:
-                check_a=prev
-            if b==c:
-                check_b=list1.next
-            c+=1
-            prev=list1
-            list1=list1.next
-        #print(check_a,check_b)
-        check_a.next=list2
-        while init.next:
-            init=init.next
-        init.next=check_b
-        return head
+        curr,head=list1,list1
+        l=b-a+1
+        print(l)
+        prev=None
+        while curr and a>0:
+            prev = curr
+            curr = curr.next
+            a-=1
+        prev.next = list2
+        
+        while curr and l>0:
+            curr = curr.next
+            l-=1
+        
+        ans = head
+        
+        while head.next:
+            head = head.next
+        head.next = curr
+        return ans
