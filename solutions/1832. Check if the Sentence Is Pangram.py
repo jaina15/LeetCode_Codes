aa28bc135ddd4421d78d3ec491a49269baafb071
Shubham Jain <shubhamjain151097@gmail.com>
2023-11-14 13:36:51 +0530

    38 ms

--- solutions/1832. Check if the Sentence Is Pangram.py
+++ solutions/1832. Check if the Sentence Is Pangram.py
@@ -1,7 +1,10 @@
+import string
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        pangram = list('thequickbrownfoxjumpsoverthelazydog')
-        for c in pangram:
-            if c not in sentence:
+        d = dict.fromkeys(string.ascii_lowercase, 0)
+        for c in sentence:
+            d[c]+=1
+        for k,v in d.items():
+            if v==0:
                 return False
         return True
