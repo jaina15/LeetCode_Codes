3f1124d0c20d34a023ff5087409dbcefde52603c
Shubham Jain <shubhamjain151097@gmail.com>
2023-11-14 13:14:43 +0530

    29 ms

--- solutions/2124. Check if All A's Appears Before All B's.py
+++ solutions/2124. Check if All A's Appears Before All B's.py
@@ -1,3 +1,12 @@
 class Solution:
     def checkString(self, s: str) -> bool:
-        return s==''.join(sorted(s))
+        ass,bss=[],[]
+        for i,c in enumerate(s):
+            if c=='a':
+                ass.append(i)
+            elif c=='b':
+                bss.append(i)
+        
+        if ass and bss and max(ass)>min(bss):
+            return False
+        return True
