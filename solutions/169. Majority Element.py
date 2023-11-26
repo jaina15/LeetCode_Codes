831c76340b8cf968ddfc49df2654bd747ac4ae64
Shubham Jain <shubhamjain151097@gmail.com>
2023-11-26 16:43:32 +0530

    156 ms

--- solutions/169. Majority Element.py
+++ solutions/169. Majority Element.py
@@ -1,3 +1,19 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return collections.Counter(nums).most_common()[0][0]
+        #Moore's voting algo
+        el,cnt=0,0
+        for i in nums:
+            if cnt==0:
+                el = i
+                cnt+=1
+            elif i==el:
+                cnt+=1
+            else:
+                cnt-=1
+        cnt1=0
+        for i in nums:
+            if i==el:
+                cnt1+=1
+        if cnt1>len(nums)/2:
+            return el
+        return -1
