d40282759de749a9c562ab35c807ced02f16b21e
Shubham Jain <shubhamjain151097@gmail.com>
2023-11-26 14:45:56 +0530

    119 ms

--- solutions/268. Missing Number.py
+++ solutions/268. Missing Number.py
@@ -1,3 +1,9 @@
 class Solution:
-    def missingNumber(self, n: List[int]) -> int:
-        return (len(n)*(len(n)+1))//2-sum(n)
+    def missingNumber(self, nums: List[int]) -> int:
+        summ = len(nums)*(len(nums)+1)//2
+        return summ-sum(nums)
+        
+        # can be done with XOR as well
+        # take xor for 1 to len(nums)
+        # take xor of all array elements
+        # return xor of above 2
