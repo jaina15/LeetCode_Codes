e589385f04d989bbc266209cfcf3124e27442400
Shubham Jain <shubhamjain151097@gmail.com>
2023-11-26 11:58:51 +0530

    55 ms

--- solutions/2460. Apply Operations to an Array.py
+++ solutions/2460. Apply Operations to an Array.py
@@ -2,11 +2,12 @@ class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
         for i in range(len(nums)-1):
             if nums[i]==nums[i+1]:
-                nums[i]=2*nums[i]
+                nums[i]=nums[i]*2
                 nums[i+1]=0
-        ans=[]
-        for i in nums:
-            if i!=0:
-                ans.append(i)
-        ans.extend([0]*(len(nums)-len(ans)))
-        return ans
+        i=0
+        for j in range(len(nums)):
+            if nums[j]!=0 and nums[i]==0:
+                nums[i],nums[j]=nums[j],nums[i]
+            if nums[i]!=0:
+                i+=1
+        return nums
