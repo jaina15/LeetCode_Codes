1f91080cc8bb9b072eb96ada2ed9acf83fcf42b1
Shubham Jain <shubhamjain151097@gmail.com>
2023-11-26 16:25:58 +0530

    48 ms

--- solutions/75. Sort Colors.py
+++ solutions/75. Sort Colors.py
@@ -3,17 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        low=mid=0
-        high=len(nums)-1
-        
+        #Dutch National Flag Algo
+        low,mid,high=0,0,len(nums)-1
         while mid<=high:
             if nums[mid]==0:
-                nums[low],nums[mid] = nums[mid],nums[low]
-                low+=1
+                nums[low],nums[mid]=nums[mid],nums[low]
                 mid+=1
+                low+=1
             elif nums[mid]==1:
                 mid+=1
             else:
-                nums[high],nums[mid] = nums[mid],nums[high]
+                nums[mid],nums[high]=nums[high],nums[mid]
                 high-=1
-        
+                
