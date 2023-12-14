88afbe4461773b98abdcb627f6da1437a51ef5a1
Shubham Jain <shubhamjain151097@gmail.com>
2023-12-14 17:00:56 +0530

    65 ms

--- solutions/383. Ransom Note.py
+++ solutions/383. Ransom Note.py
@@ -1,19 +1,10 @@
+from collections import Counter
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ran,mag=dict(),dict()
-        for i in ransomNote:
-            if i in ran:
-                ran[i]+=1
-            else:
-                ran[i]=1
-        for i in magazine:
-            if i in mag:
-                mag[i]+=1
-            else:
-                mag[i]=1
-        for k,v in ran.items():
-            if k not in mag:
-                return False
-            elif ran[k]>mag[k]:
+        cnt_a = Counter(ransomNote)
+        cnt_b = Counter(magazine)
+        
+        for k,v in cnt_a.items():
+            if v>cnt_b[k] or k not in cnt_b:
                 return False
         return True
