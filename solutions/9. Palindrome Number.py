e9efc6d143bc5016a8e6e083dff48401c14161ef
Shubham Jain <shubhamjain151097@gmail.com>
2024-01-02 17:24:06 +0530

    135 ms

--- solutions/9. Palindrome Number.py
+++ solutions/9. Palindrome Number.py
@@ -2,7 +2,7 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         n,j=x,0
         while n>0:
-            r = n%10
-            j = j*10 + r
+            r=n%10
+            j=j*10+r
             n//=10
         return j==x
