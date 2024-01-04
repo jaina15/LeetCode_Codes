8b989968b554d90a0dc49aaa01f28cc148cf74b2
Shubham Jain <shubhamjain151097@gmail.com>
2024-01-04 18:24:08 +0530

    414 ms

--- solutions/18. 4Sum.py
+++ solutions/18. 4Sum.py
@@ -8,11 +8,10 @@ class Solution:
             for j in range(i+1,len(nums)-2):
                 if j>i+1 and nums[j]==nums[j-1]:
                     continue
-                tsum=target-(nums[i]+nums[j])
-                lo=j+1
-                hi=len(nums)-1
+                tsum = target-(nums[i]+nums[j])
+                lo,hi=j+1,len(nums)-1
                 while lo<hi:
-                    if tsum==nums[lo]+nums[hi]:
+                    if tsum == nums[lo]+nums[hi]:
                         ans.append([nums[i],nums[j],nums[lo],nums[hi]])
                         while lo<hi and nums[lo]==nums[lo+1]:
                             lo+=1
@@ -20,7 +19,7 @@ class Solution:
                             hi-=1
                         lo+=1
                         hi-=1
-                    elif tsum>nums[lo]+nums[hi]:
+                    elif tsum > nums[lo]+nums[hi]:
                         lo+=1
                     else:
                         hi-=1
