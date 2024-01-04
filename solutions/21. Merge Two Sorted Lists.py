03063743c93d85048ad14e6c55750bd54aebb046
Shubham Jain <shubhamjain151097@gmail.com>
2024-01-04 18:37:48 +0530

    34 ms

--- solutions/21. Merge Two Sorted Lists.py
+++ solutions/21. Merge Two Sorted Lists.py
@@ -10,11 +10,10 @@ class Solution:
         while list1 and list2:
             if list1.val<list2.val:
                 curr.next = list1
-                list1,curr = list1.next, list1
+                list1,curr = list1.next,list1
             else:
                 curr.next = list2
-                list2,curr = list2.next, list2
-        
+                list2,curr = list2.next,list2
         if list1 or list2:
             curr.next = list1 if list1 else list2
         return ans.next
