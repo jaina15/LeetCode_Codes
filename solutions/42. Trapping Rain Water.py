e38bb30bb1f34840248787e3d4e9f1ec231bd51f
Shubham Jain <shubhamjain151097@gmail.com>
2024-02-22 17:28:35 +0530

    100 ms

--- solutions/42. Trapping Rain Water.py
+++ solutions/42. Trapping Rain Water.py
@@ -1,9 +1,8 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        l,r = 0,len(height)-1
+        l,r=0,len(height)-1
         lmax,rmax = height[l],height[r]
         res=0
-        
         while l<r:
             if lmax<rmax:
                 l+=1
