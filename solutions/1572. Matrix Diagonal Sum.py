c5d780ad8664efe42e50156e8e1434c0baffde51
Shubham Jain <shubhamjain151097@gmail.com>
2024-02-23 12:10:35 +0530

    95 ms

--- solutions/1572. Matrix Diagonal Sum.py
+++ solutions/1572. Matrix Diagonal Sum.py
@@ -1,7 +1,11 @@
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
-        ans=0
-        for i in range(len(mat)):
-            #print(mat[i][i],mat[i][len(mat)-i-1])
-            ans+=mat[i][i]+mat[i][len(mat)-i-1]
-        return ans-mat[len(mat)//2][len(mat)//2] if len(mat)%2==1 else ans
+        ans = 0
+        n=len(mat)
+        for i in range(n):
+            ans+=mat[i][i]
+            ans+=mat[n-1-i][i]
+        
+        if n%2!=0:
+            ans-=mat[n//2][n//2]
+        return ans
