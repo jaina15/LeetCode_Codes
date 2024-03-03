58ba2136ed19c942fca411ac09bc0ec7d8e74518
Shubham Jain <shubhamjain151097@gmail.com>
2024-03-03 19:26:05 +0530

    33 ms

--- solutions/226. Invert Binary Tree.py
+++ solutions/226. Invert Binary Tree.py
@@ -8,11 +8,5 @@ class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
             return None
-        
-        temp = root.left
-        root.left = root.right
-        root.right = temp
-        
-        self.invertTree(root.left)
-        self.invertTree(root.right)
+        root.left,root.right = self.invertTree(root.right),self.invertTree(root.left)
         return root
