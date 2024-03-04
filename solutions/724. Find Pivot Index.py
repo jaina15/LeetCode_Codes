4869c574919373be2dfdfa964bf781e53354dd7c
Shubham Jain <shubhamjain151097@gmail.com>
2024-03-04 17:13:18 +0530

    114 ms

---
+++ solutions/724. Find Pivot Index.py
@@ -0,0 +1,9 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        l,r=0,sum(nums)
+        for i,num in enumerate(nums):
+            r-=num
+            if l==r:
+                return i
+            l+=num
+        return -1
