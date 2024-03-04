ba065b3a622b3e0689fedb374af991e6ca67b522
Shubham Jain <shubhamjain151097@gmail.com>
2024-03-04 17:08:30 +0530

    64 ms

--- solutions/2574. Left and Right Sum Differences.py
+++ solutions/2574. Left and Right Sum Differences.py
@@ -1,10 +1,16 @@
 class Solution:
-    def leftRigthDifference(self, nums: List[int]) -> List[int]:
-        suffix = sum(nums)
-        ans=[]
-        prefix=0
-        for x in nums:
-            prefix+=x
-            ans.append(abs(suffix-prefix))
-            suffix-=x
+    def leftRightDifference(self, nums: List[int]) -> List[int]:
+        lsum,rsum=[0],[0]
+        csum=0
+        for i in range(1,len(nums)):
+            csum+=nums[i-1]
+            lsum.append(csum)
+        csum=0
+        for i in range(len(nums)-1,0,-1):
+            csum+=nums[i]
+            rsum.append(csum)
+        rsum=rsum[::-1]
+        ans=[0]*len(nums)
+        for i in range(len(nums)):
+            ans[i]=abs(lsum[i]-rsum[i])
         return ans
